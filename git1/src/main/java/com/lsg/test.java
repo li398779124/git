1b4180b4974761cf551dafecc687605453b9de97
li398779124 <dawn_li1996@qq.com>
2020-08-04 19:46:14 +0800

    dawn

--- git1/src/main/java/com/lsg/test.java
+++ git1/src/main/java/com/lsg/test.java
@@ -2,6 +2,6 @@ package com.lsg;
 
 public class test {
     public static void main(String [] args){
-        System.out.println("ss");
+        System.out.println("ss熟悉熟悉实习生的反馈艰苦艰苦就是对方");
     }
 }
